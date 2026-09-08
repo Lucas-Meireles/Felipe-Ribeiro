94e9107fdad880f9a5ec733bb7cd88403abbee50
Lucas-Meireles <lucas.santana962@gmail.com>
2026-09-08 10:47:19 -0300

    Corrige áreas de atuação em telas intermediárias

--- src/App.jsx
+++ src/App.jsx
@@ -1394,9 +1394,7 @@ ${data.get('mensagem')}`;
              </em>
             </h2>
            </div>
-           <p>
-            Espaço reservado para inserir imagens de avaliações reais de clientes, mantendo a apresentação visual discreta e alinhada à identidade do escritório.
-           </p>
+
           </div>
           <div
            aria-label="Galeria de avaliações de clientes"
